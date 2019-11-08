0121c765c181f7032df02fe136474c65a3288b8e
ibnufaisal-27 <mibnufaisal@gmail.com>
2019-11-08 13:52:41 +0700

    Ecommerce With Laravel 6

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+//JADI INI GROUPING ROUTE, SEHINGGA SEMUA ROUTE YANG ADA DIDALAMNYA
+//SECARA OTOMATIS AKAN DIAWALI DENGAN administrator
+//CONTOH: /administrator/category ATAU /administrator/product, DAN SEBAGAINYA
+Route::group(['prefix' => 'administrator', 'middleware' => 'auth'], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('/category', 'CategoryController')->except('create', 'show');
+    Route::resource('product', 'ProductController')->except(['show']); //BAGIAN INI KITA TAMBAHKAN EXCETP KARENA METHOD SHOW TIDAK DIGUNAKAN
+    Route::get('/product/bulk', 'ProductController@massUploadForm')->name('product.bulk'); //TAMBAHKAN ROUTE INI;
+    Route::post('/product/bulk', 'ProductController@massUpload')->name('product.saveBulk');
+});
